5a9d50acadadb94c4e3533edab890c8b3f6f2613
Kate F <follett.katie@gmail.com>
2019-01-08 21:15:15 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -2,7 +2,7 @@
 def levitation_quiz
 	puts "What is the spell that enacts levitation?"
 	spell = gets
-	puts "What is the #{spell} that enacts levitation?"
+
 	if spell == "Wingardium Leviosa"
   puts "You passed the quiz!"
 end
